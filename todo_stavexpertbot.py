dbb4f7d3e80650c710dfeb3313970ceb775608ff
antonio-st <antonio-st@yandex.ru>
2022-01-10 23:08:11 +0300

    update todo bot command

--- todo_stavexpertbot.py
+++ todo_stavexpertbot.py
@@ -19,7 +19,10 @@ HELP = '''
 tasks = {}
 
 todayDate = datetime.date.today().strftime("%d.%m.%Y")
-print('TODAY : ', todayDate)
+print('Start Bot: ', todayDate)
+
+
+
 
 randomTasks = ['do morning exercises', 'walk to the Konsomolsky pond', 'an evening walk', 'clean up the yard',
                'learning English']
@@ -49,27 +52,38 @@ def help(message):
 # decorator function to handle all text message: '/add'
 @bot.message_handler(commands=['add'])
 def add(message):
-    command = message.text.split(maxsplit=2)
-    date = command[1]
-    task = command[2]
-    add_todo(date, task)
-    repToChat = 'Task ' + task + ' on ' + date + ' is recorded'
-    bot.send_message(message.chat.id, repToChat)
+    commandOrig = message.text
+    if commandOrig == '/add': # a command without date and task has been introduced
+        repToChat = '$ Please enter a command in the format:' + '\n' + '/add xx.xx.xxxx your task'
+        bot.send_message(message.chat.id, repToChat)
+    else:
+        command = message.text.split(maxsplit=2)
+        date = command[1]
+        task = command[2]
+        if len(task) <= 3: # short task, less than 3 characters
+            repToChat = '$ The task is too short: please enter text more than 3 characters' + '\n'
+            bot.send_message(message.chat.id, repToChat)
+        else:
+            add_todo(date, task)
+            repToChat = '$ Task ' + '\'' + task + '\'' + ' on ' + date + ' is recorded'
+            bot.send_message(message.chat.id, repToChat)
+
 
 
 # decorator function to handle all text message: '/random'
 @bot.message_handler(commands=['random'])
 def randomTask(message):
-    if todayDate in tasks:
-        print('There are already task today: ')
-        bot.send_message(message.chat.id, 'There are already task today: ')
-        for task in tasks[todayDate]:
+    todayDatelocal = datetime.date.today().strftime("%d.%m.%Y")
+    if todayDatelocal in tasks:
+        print('$ There are already task today: ')
+        bot.send_message(message.chat.id, '$ There are already task today: ')
+        for task in tasks[todayDatelocal]:
             bot.send_message(message.chat.id, '- ' + task)
             print('- ', task)
     else:
         task = random.choice(randomTasks)
         add_todo(todayDate, task)
-        repToChat = 'Task ' + '\'' + task + '\'' + ' on ' + todayDate + ' is recorded'
+        repToChat = '$ Task ' + '\'' + task + '\'' + ' on ' + todayDate + ' is recorded'
         bot.send_message(message.chat.id, repToChat)
 
 
@@ -77,19 +91,42 @@ def randomTask(message):
 @bot.message_handler(commands=['show', 'print'])
 def show(message):
     command = message.text.split(maxsplit=1)
-    date = command[1].lower()
-    if date in tasks:
-        text = date.upper() + '\n'
-        bot.send_message(message.chat.id, 'on date: ' + date + '\n')
-        print('on date: ', date, '\n')
-        for task in tasks[date]:
-            # text = text + '- ' + tasks + '\n'
-            bot.send_message(message.chat.id, '- ' + task)
-            print('- ', task)
+    commandOrig = message.text
+    todayDatelocal = datetime.date.today().strftime("%d.%m.%Y")
+    if commandOrig in '/show':
+        if len(tasks) == 0:
+            print('$ For ', todayDatelocal, 'tasks not found', '\n')
+            text = '$ For ' + todayDatelocal + ' tasks not found' + '\n'
+            bot.send_message(message.chat.id, text)
+        else:
+            bot.send_message(message.chat.id, '$ You task for the date: ' + todayDatelocal + '\n')
+            print('$ You task for the date:  ', todayDatelocal, '\n')
+            for task in tasks[todayDatelocal]:
+                bot.send_message(message.chat.id, '- ' + task)
+                print('- ', task)
+
     else:
-        print('For ', date, 'tasks not found', '\n')
-        text = 'For ' + date + ' tasks not found' + '\n'
-        bot.send_message(message.chat.id, text)
+        date = command[1].lower()
+        if date in tasks:
+            text = date.upper() + '\n'
+            bot.send_message(message.chat.id, '$ You task for the date: ' + date + '\n')
+            print('$ You task for the date: ', date, '\n')
+            for task in tasks[date]:
+                # text = text + '- ' + tasks + '\n'
+                bot.send_message(message.chat.id, '- ' + task)
+                print('- ', task)
+        else:
+            print('$ For ', date, 'tasks not found', '\n')
+            text = '$ For ' + date + ' tasks not found' + '\n'
+            bot.send_message(message.chat.id, text)
+
+# decorator function for any text, search for certain words and answer them
+@bot.message_handler(content_types=['text'])
+def greeting(message):
+    if message.text == 'hello'.lower() or 'привет'.lower():
+        bot.reply_to(message, text='$ hello '+ message.chat.first_name + ' ! ' + '\n'
+                     + '$ to find out the command type /help ')
+
 
 
 # constantly access telegram servers
